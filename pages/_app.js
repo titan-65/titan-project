331fb01404aade47699caaa3a09b6ef631ca4b4f
Vantol Bennett <vvantol2000@icloud.com>
2020-10-03 21:27:33 -0500

    additon of components

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../styles/css/style.css'
+import '../styles/css/vendor/bootstrap.min.css'
+import '../styles/css/vendor/bootstrap.min.css'
+import '../styles/css/vendor/fontawesome.css'
+import '../styles/css/vendor/lightbox.css'
+import '../styles/css/plugins/plugins.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
